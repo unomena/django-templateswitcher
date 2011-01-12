44941a31d699d3ca77ec15a0d2a68276b04edf2b
euan <euan@unomena.com>
2011-01-12 12:02:31 +0200

    Upgraded to new ua_mapper.

--- templateswitcher/middleware.py
+++ templateswitcher/middleware.py
@@ -7,7 +7,7 @@ from django.core.cache import cache
 
 from templateswitcher.views import flatpageview
 
-from mobile.sniffer.chain import ChainedSniffer
+#from mobile.sniffer.chain import ChainedSniffer
 
 #==============================================================================
 class TemplateDirSwitcher(object):
@@ -33,35 +33,38 @@ class TemplateDirSwitcher(object):
                         full_path.startswith(settings.ADMIN_MEDIA_PREFIX) or full_path.startswith('/favicon'))
         
         if not template_set:
-            # set the device switcher library according to the settings - defaults to wurfl
-            device_switch_libs = getattr(settings, 'DEVICE_SWITCH_LIB', ['WurlfSniffer'])
-            da_api = getattr(settings, 'DEVICE_ATLAS_API_FILE', None)
-            device_cache_timeout = getattr(settings, 'DEVICE_CACHE_TIMEOUT', 72000)
-                
-            # import requied device library classes
-            if 'ApexVertexSniffer' in device_switch_libs:
-                from mobile.sniffer.apexvertex.sniffer import ApexVertexSniffer
-            if 'WAPProfileSniffer' in device_switch_libs:
-                from mobile.sniffer.wapprofile.sniffer import WAPProfileSniffer
-            if 'DeviceAtlasSniffer' in device_switch_libs:
-                if not os.path.exists(da_api):
-                    raise Exception('DEVICE_ATLAS_API_FILE must be in your setting and contain a valid path to use Device Atlas.')
-                from mobile.sniffer.deviceatlas.sniffer import DeviceAtlasSniffer
-            if 'WurlfSniffer' in device_switch_libs:
-                from mobile.sniffer.wurlf.sniffer import WurlfSniffer
             
-            chained_libs = []
-            for device_lib in device_switch_libs:
-                chained_libs.append(eval(device_lib)())
+            template_set = request.META['TEMPLATE_SET']
             
-            # instantiate the sniffer and device object
-            sniffer = ChainedSniffer(chained_libs)
-            device_object = sniffer.sniff(request)
-            template_set = device_families.get_device_family(device_object)
-                
-            # copy the device object to the request object
-            request.device = device_object
-            cache.set(device_cache_key, template_set, device_cache_timeout)
+#            # set the device switcher library according to the settings - defaults to wurfl
+#            device_switch_libs = getattr(settings, 'DEVICE_SWITCH_LIB', ['WurlfSniffer'])
+#            da_api = getattr(settings, 'DEVICE_ATLAS_API_FILE', None)
+#            device_cache_timeout = getattr(settings, 'DEVICE_CACHE_TIMEOUT', 72000)
+#                
+#            # import requied device library classes
+#            if 'ApexVertexSniffer' in device_switch_libs:
+#                from mobile.sniffer.apexvertex.sniffer import ApexVertexSniffer
+#            if 'WAPProfileSniffer' in device_switch_libs:
+#                from mobile.sniffer.wapprofile.sniffer import WAPProfileSniffer
+#            if 'DeviceAtlasSniffer' in device_switch_libs:
+#                if not os.path.exists(da_api):
+#                    raise Exception('DEVICE_ATLAS_API_FILE must be in your setting and contain a valid path to use Device Atlas.')
+#                from mobile.sniffer.deviceatlas.sniffer import DeviceAtlasSniffer
+#            if 'WurlfSniffer' in device_switch_libs:
+#                from mobile.sniffer.wurlf.sniffer import WurlfSniffer
+#            
+#            chained_libs = []
+#            for device_lib in device_switch_libs:
+#                chained_libs.append(eval(device_lib)())
+#            
+#            # instantiate the sniffer and device object
+#            sniffer = ChainedSniffer(chained_libs)
+#            device_object = sniffer.sniff(request)
+#            template_set = device_families.get_device_family(device_object)
+#                
+#            # copy the device object to the request object
+#            request.device = device_object
+#            cache.set(device_cache_key, template_set, device_cache_timeout)
             
         if not media_request:
             # switch the template dir for the given device
